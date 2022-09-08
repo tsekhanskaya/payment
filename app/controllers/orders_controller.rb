3cf6df93a932bb8a3a0c107496e9da9ad892143f
tsekhanskaya <anutta2222@gmail.com>
2022-09-08 12:04:47 +0300

    Added order controller and index.html

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    products = Product.all
+    @products_purchase = products.where(stripe_plan_name: nil, paypal_plan_name: nil)
+    @products_subscription = products - @products_purchase
+  end
+
+  def submit; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :homes
+  # resources :homes
   devise_for :users
-  root to: 'homes#index'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  # root to: 'homes#index'
+  get '/', to: 'orders#index'
+  post '/orders/submit', to: 'orders#submit'
 end
