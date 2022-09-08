1855e39c665785784a0fb5a07d36121dcf2579f4
tsekhanskaya <anutta2222@gmail.com>
2022-09-08 12:31:39 +0300

    Payment Gateway Credentials Storage

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,9 @@ module ExampleWithPaypal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config_file = Rails.application.config_for(:application)
+    config_file.each do |key,value|
+      ENV[key] = value
+    end unless config_file.nil?
   end
 end
